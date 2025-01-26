e8dda756dc8ebe568e54f4152260865dbd4bd7bf
James Camilleri <james@james.mt>
2025-01-26 21:19:19 +0000

    Handle transition order/timeouts automatically

--- packages/create-website/templates/svelte-kit/src/lib/components/transition/Transition.svelte
+++ packages/create-website/templates/svelte-kit/src/lib/components/transition/Transition.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte'
+
   import { fade } from 'svelte/transition'
 
-  export let order = 0
+  import { navigating } from '$app/state'
+
+  interface Props {
+    order?: number
+    group?: number
+    children: Snippet
+  }
+
+  let { order: manualOrder, group, children }: Props = $props()
+
+  let element = $state<HTMLElement>()
+  let order = $derived(manualOrder ?? (element && calculateOffset(element, group)))
+  let delay = $derived(order && 100 * order)
 
   const duration = 300
-  const delay = 100 * order
+
+  function calculateOffset(element: HTMLElement, group?: number) {
+    let match = element
+    if (group) {
+      const firstInGroup = document.querySelector(
+        `.transition-block[data-group="${group}"]`,
+      ) as HTMLElement
+
+      match = firstInGroup
+    }
+
+    return [...document.querySelectorAll('.transition-block').values()].indexOf(match)
+  }
 </script>
 
-<div in:fade={{ duration, delay }}>
-  <slot />
-</div>
+{#key order}
+  <div
+    in:fade={{
+      delay,
+      // Only animate if an internal navigation takes place.
+      // This avoids the elements blinking out of existence
+      // and then animating back in on first load.
+      duration: navigating.type ? duration : 0
+    }}
+    class="transition-block"
+    data-group={group}
+    bind:this={element}
+  >
+    {@render children()}
+  </div>
+{/key}
