8d44db012fa739f0c58dfd5b1a62751e320ed391
James Camilleri <james@james.mt>
2026-08-25 22:16:46 +0100

    🔧 Update import sort order

--- packages/config/_sort-order.js
+++ packages/config/_sort-order.js
@@ -1,5 +1,15 @@
 // This is shared between oxfmt and eslint, so files that can't
 // be formatted with oxfmt will have the same sort order.
 export const sortImports = {
-  groups: ['builtin', 'external', 'internal', 'unknown', 'parent', 'sibling', 'index', 'style'],
+  groups: [
+    'type',
+    'builtin',
+    'external',
+    'internal',
+    'unknown',
+    'parent',
+    'sibling',
+    'index',
+    'style',
+  ],
 }
