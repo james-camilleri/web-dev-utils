c19f5c87ee1e000d34e7940b4f475cc64cb504c4
James Camilleri <james@james.mt>
2026-08-27 11:56:21 +0100

    🔧 Enable svelte formatting on oxfmt

--- packages/config/oxfmt.config.js
+++ packages/config/oxfmt.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'oxfmt'
 import { sortImports } from './_sort-order.js'
 
 export default defineConfig({
+  ignorePatterns: ['**/.astro/', '**/.svelte-kit/', '**/.changeset/', '**/dist/', '**/.sanity/'],
+
   semi: false,
   singleQuote: true,
-
-  ignorePatterns: ['**/.astro/', '**/.svelte-kit/', '**/.changeset/', '**/dist/', '**/.sanity/'],
+  svelte: true,
 
   sortImports,
 })
